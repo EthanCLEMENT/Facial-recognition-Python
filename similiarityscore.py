e7ae6a15b1e8df5747ef163d646287da0217ae7e
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-02-01 22:37:32 +0100

    Update similiarityscore.py

--- similiarityscore.py
+++ similiarityscore.py
@@ -1,7 +1,5 @@
 from __future__ import print_function, unicode_literals
-
 import json
-
 from facepplib import FacePP, exceptions
 
 face_detection=""
@@ -16,13 +14,10 @@ def face_comparing(app):
 
     img_url1 = "https://i.postimg.cc/J4CrR1kf/test3.jpg"
     img_url2 = "https://i.postimg.cc/J4CrR1kf/test3.jpg"
-
     cmp_ = app.compare.get(image_url1=img_url1,image_url2=img_url2)
 
     print('image1', '=', cmp_.image1)
     print('image2', '=', cmp_.image2)
-
-    print('thresholds', '=', json.dumps(cmp_.thresholds, indent=4))
     print('confidence', '=', cmp_.confidence)
 
 if __name__ == '__main__':
