b3b6e8cdaf4317018078f94f2194d09484074bd0
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-02-01 22:42:10 +0100

    Update genderprediction.py

--- genderprediction.py
+++ genderprediction.py
@@ -1,46 +1,39 @@
-## Load the gender predicition model
+# import libraries
 import cv2
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
 
-
 #load model
 model = load_model("gender_predictor.model")
-
 cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 #defining classes
 classes = ['men','women']
 
 def gender_facecounter(image, m, f, size=0.5):
-    ## convert image into gray scaled image
+    # convert image into gray scaled image
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faces = cascade.detectMultiScale(gray_image, 1.1,5)
-    ## iterating over faces
-
+    # iterating over faces
     for (x,y,w,h) in faces:
         cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0),3)
-
         cropped_image = np.copy(image[y:y+h,x:x+w])
 
-        ## preprocess the image according to our model
+        # preprocess the image according to our model
         res_face = cv2.resize(cropped_image, (96,96))
-        ## cv2.imshow("cropped image",res_face)
+        # cv2.imshow("cropped image",res_face)
         res_face = res_face.astype("float") / 255.0
         res_face = img_to_array(res_face)
         res_face = np.expand_dims(res_face, axis=0)
-
-
-        ## model prediction
+       
+        # model prediction
         result = model.predict(res_face)[0]
-
-        ## get label with max accuracy
+        # get label with max accuracy
         idx = np.argmax(result)
         label = classes[idx]
-
-        ## calculating count
+        # calculating count
         if label == "women":
             f = f+1
         else:
@@ -54,10 +47,10 @@ def gender_facecounter(image, m, f, size=0.5):
 
     return image
 
-## loading an image
+# loading an image
 image = cv2.imread(r"C:\Users\ethan\Downloads\test6.jpg") #path to image
 
-## maintaining separate counters
+# maintaining separate counters
 males = 0
 females = 0
 
